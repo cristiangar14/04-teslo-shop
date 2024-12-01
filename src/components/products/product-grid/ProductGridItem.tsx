85ede5ab049618d66b9becfced4ef9cc7ec2634a
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-12-01 12:28:05 -0500

    feat(add): adding the hover effect of the images in the cards

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,22 +1,33 @@
+'use client';
+
 import { Product } from "@/interfaces"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   product: Product;
 }
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState( product.images[0]);
+
+  const handleImgHover = (index: number) => {
+    setDisplayImage(product.images[index]);
+  }
+
   return (
     <div
       className="rounded-md overflow-hidden fade-in"
     >
       <Link href={`/product/${product.slug}`}>
         <Image
-          src={`/products/${ product.images[0] }`}
+          src={`/products/${ displayImage }`}
           alt={ product.title}
-          className="w-full object-cover"
+          className="w-full object-cover rounded"
           width={500}
           height={500}
+          onMouseEnter={() => handleImgHover(1)}
+          onMouseOut={() => handleImgHover(0)}
         />
       </Link>
       <div className="p-4 flex flex-col">
