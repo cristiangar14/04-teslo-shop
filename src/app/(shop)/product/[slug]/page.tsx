2aa90e472a87379de7a2d78d4037ab7e5a76f32b
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2024-12-17 22:25:12 -0500

    feat(add): starting with product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,43 @@
+import { titleFont } from "@/config/font";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
 
-export default function ProductPage() {
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params
+  const product = initialData.products.find(product => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/**slideshow */}
+      <div className="col-span-1 md:col-span-2">hohl</div>
+      {/**description */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+        {/* tallas */}
+        {/* cantidad */}
+        {/* boton */}
+        <button className="btn-primary my-5">
+          Agregr al carrito
+        </button>
+
+        {/* Descripcion */}
+        <h3 className="font-bold text-sm">Descripcion</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
