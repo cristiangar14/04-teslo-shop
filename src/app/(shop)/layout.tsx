f0c2390f743f38f87bc3d4a17e0c007be0e8cfa5
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2025-01-15 15:32:27 -0500

    feat(add): implement footer component and integrate it into shop layout

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { Sidebar, TopMenu } from "@/components";
+import { Footer, Sidebar, TopMenu } from "@/components";
 
 export default function ShopLayout({
   children,
@@ -11,5 +11,6 @@ export default function ShopLayout({
     <div className="px-0 sm:px-10">
       {children}
     </div>
+    <Footer />
     </main>;
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,3 +2,4 @@ export { PageNotFound } from './not-found/PageNotFound';
 export { Title } from './title/Title';
 export { TopMenu } from './top-menu/TopMenu';
 export { Sidebar } from './sidebar/Sidebar';
+export { Footer } from './footer/Footer';
