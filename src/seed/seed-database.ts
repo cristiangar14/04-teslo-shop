59a2525308bd0eee970fed940cd49bf529371487
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2025-01-16 00:17:30 -0500

    feat(add): integrate PostgreSQL database with Prisma and seed data

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,58 @@
+import { initialData } from "./seed";
+import  prisma from '../lib/prisma';
+
+async function main() {
+  const { categories, products } = initialData;
+  // Borrar registros previos 
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  //categories
+  const categoriesData =  categories.map( name => ({name}));
+  await prisma.category.createMany({
+    data: categoriesData
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>) // <string=shirt, string=categoriaId>;
+  
+  // Products
+  products.forEach(async(product) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {images, type, ...rest} = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type.toLowerCase()]
+
+      }
+    });
+
+    //images
+
+    const imagesData = images.map(image => ({
+      url: image,
+      productId: dbProduct.id
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData
+    });
+  });
+
+
+
+  console.log('Seed ejecutado correctamente')
+};
+
+
+(()=>{
+
+  if ( process.env.NODE_ENV === 'production' ) return;
+
+  main();
+})();
