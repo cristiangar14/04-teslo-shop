20a4873352bd5e68139531c0a3040ed1bf032f46
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2025-01-21 18:48:31 -0500

    fix: correct string literal syntax and improve JSX formatting in StockLabel component

--- src/components/product/stock-label/StockLabel.tsx
+++ src/components/product/stock-label/StockLabel.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { getStockBySlug } from "@/actions";
 import { titleFont } from "@/config/font";
 import { useEffect, useState } from "react";
@@ -12,27 +12,27 @@ export const StockLabel = ({ slug }: Props) => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
     getStock();
-  },[]);
-  
-  const getStock = async () => { 
+  }, []);
+
+  const getStock = async () => {
     const stock = await getStockBySlug(slug);
     setStock(stock);
     setIsLoading(false);
-   };
+  };
 
   return (
-    <> 
-      {
-        isLoading ? 
-        <h2 className={`${titleFont.className} antialiased font-bold text-lg bg-gray-200 animate-pulse`}>
-            &nbsp;
-      </h2> 
-      : <h2 className={`${titleFont.className} antialiased font-bold text-lg`}>
-            Stock: { stock }
-      </h2>
-      }
-      
-      
+    <>
+      {isLoading ? (
+        <h2
+          className={`${titleFont.className} antialiased font-bold text-lg bg-gray-200 animate-pulse`}
+        >
+          &nbsp;
+        </h2>
+      ) : (
+        <h2 className={`${titleFont.className} antialiased font-bold text-lg`}>
+          Stock: {stock}
+        </h2>
+      )}
     </>
-  )
-}
+  );
+};
