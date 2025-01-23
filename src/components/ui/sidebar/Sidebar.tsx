07ef35eb918a1e53178209ca82ff391371940f33
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2025-01-23 14:57:43 -0500

    feat(add): implement logout functionality and update sidebar with logout button

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,130 +1,121 @@
-'use client'
-import { useUIStore } from "@/store"
-import clsx from "clsx"
-import Link from "next/link"
-import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
+"use client";
+import { logout } from "@/actions";
+import { useUIStore } from "@/store";
+import clsx from "clsx";
+import Link from "next/link";
+import {
+  IoCloseOutline,
+  IoLogInOutline,
+  IoLogOutOutline,
+  IoPersonOutline,
+  IoSearchOutline,
+  IoShirtOutline,
+  IoTicketOutline,
+} from "react-icons/io5";
 
 export const Sidebar = () => {
-
-  const isSideMenuOpen: boolean =  useUIStore( state => state.isSideMenuOpen );
-  const closeSideMenu: () => void =  useUIStore( state => state.closeSideMenu );
+  const isSideMenuOpen: boolean = useUIStore((state) => state.isSideMenuOpen);
+  const closeSideMenu: () => void = useUIStore((state) => state.closeSideMenu);
 
   return (
     <div className="">
-
       {/*Background black*/}
-      {
-        isSideMenuOpen && (
-          <div onClick={closeSideMenu} className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"></div>
-
-        )
-      }
+      {isSideMenuOpen && (
+        <div
+          onClick={closeSideMenu}
+          className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"
+        ></div>
+      )}
 
       {/*Background blur*/}
-      {
-        isSideMenuOpen && (
-          <div onClick={closeSideMenu} className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"></div>
-        )
-      }
+      {isSideMenuOpen && (
+        <div
+          onClick={closeSideMenu}
+          className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm"
+        ></div>
+      )}
       {/*Sidemenu*/}
       <nav
-        className={
-          clsx( 
-            "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
-            {
-              "translate-x-full": !isSideMenuOpen
-            }
-          )
-        }
+        className={clsx(
+          "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+          {
+            "translate-x-full": !isSideMenuOpen,
+          }
+        )}
       >
-        <IoCloseOutline 
+        <IoCloseOutline
           size={50}
           className="absolute top-5 right-5 cursor-pointer"
           onClick={closeSideMenu}
         />
         {/*Input*/}
         <div className="relative mt-14">
-          <IoSearchOutline
-            size={20}
-            className="absolute top-2 left-2"
-          />
-          <input 
+          <IoSearchOutline size={20} className="absolute top-2 left-2" />
+          <input
             type="text"
             placeholder="Buscar"
             className="w-full bg-gray-50 rounded pl-10 py-1 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
-            name="" 
-            id="" />
+            name=""
+            id=""
+          />
         </div>
 
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/profile'}
+          href={"/profile"}
           onClick={closeSideMenu}
         >
-          <IoPersonOutline
-            size={30}
-          />
+          <IoPersonOutline size={30} />
           <span className="ml-3 text-xl">Perfil</span>
         </Link>
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+          href={"/"}
         >
-          <IoTicketOutline
-            size={30}
-          />
+          <IoTicketOutline size={30} />
           <span className="ml-3 text-xl">Ordenes</span>
         </Link>
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+          href={"/auth/login"}
         >
-          <IoLogInOutline
-            size={30}
-          />
+          <IoLogInOutline size={30} />
           <span className="ml-3 text-xl">Ingresar</span>
         </Link>
-        <Link
-          className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+
+        <button
+          onClick={() =>logout()}
+          className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
         >
-          <IoLogOutOutline
-            size={30}
-          />
+          <IoLogOutOutline size={30} />
           <span className="ml-3 text-xl">Salir</span>
-        </Link>
+        </button>
 
         {/*Line Separator */}
         <div className="w-full h-px bg-gray-200 my-10" />
 
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+          href={"/"}
         >
-          <IoShirtOutline
-            size={30}
-          />
+          <IoShirtOutline size={30} />
           <span className="ml-3 text-xl">Productos</span>
         </Link>
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+          href={"/"}
         >
-          <IoTicketOutline
-            size={30}
-          />
+          <IoTicketOutline size={30} />
           <span className="ml-3 text-xl">Ordenes</span>
         </Link>
         <Link
           className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
-          href={'/'}
+          href={"/"}
         >
-          <IoPersonOutline
-            size={30}
-          />
+          <IoPersonOutline size={30} />
           <span className="ml-3 text-xl">Usuarios</span>
         </Link>
       </nav>
     </div>
-  )
-}
+  );
+};
